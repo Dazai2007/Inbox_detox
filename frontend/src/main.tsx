9aede8122502a9d86e46b741870d48942399df0f
saleh abbaszade <salehabbaszade113@gmail.com>
2025-10-28 21:33:58 +0400

    feat: update Nexivo AuthPage UI and logic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import './styles.css'
 import ErrorBoundary from './components/ErrorBoundary'
 import { I18nProvider } from './context/I18nContext'
